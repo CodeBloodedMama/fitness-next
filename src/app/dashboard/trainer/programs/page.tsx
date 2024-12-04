c7e5cb49b353fc3561d720d3022c16131a36d722
CodeBloodedMama <elisabethbinzer@hotmail.com>
2024-12-04 12:50:28 +0100

    .

--- src/app/dashboard/trainer/programs/page.tsx
+++ src/app/dashboard/trainer/programs/page.tsx
@@ -5,7 +5,7 @@ import ProgramsPageContent from "./programspageContent";
 
 export default function ProgramsPage() {
   return (
-    <Suspense fallback={<div className="flex justify-center items-center h-48">Loading programs...</div>}>
+    <Suspense fallback={<div className="flex justify-center items-center h-48">Loading programs....</div>}>
       <ProgramsPageContent />
     </Suspense>
   );
